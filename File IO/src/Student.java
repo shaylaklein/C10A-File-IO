8f50628283b2aee86f971c51e05f6cdc98864d86
Shayla Klein <shaylaklein@mymail.mines.edu>
2018-09-27 14:32:10 -0600

    first commit

---
+++ File IO/src/Student.java
@@ -0,0 +1,20 @@
+
+public class Student implements Comparable<Student> {
+
+	private String name; 
+	private int score;
+	
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	} 
+	
+	@Override
+	public int compareTo (Student o) {
+		
+	}
+	
+	
+	
+}
